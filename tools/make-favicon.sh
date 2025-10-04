46d5cdc4047fa8bc3c2701c0a2d6d1b27982ad66
bruce <bruce@workmac.localdomain.com>
2025-10-04 14:28:01 +0800

    added make-favicon script and newly generated icons

--- tools/make-favicon.sh
+++ tools/make-favicon.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Ensure only valid image formats are provided
+FAVICON_FOLDER="icons_to_put_in_assets_img"
+
+# Ensure FAVICON_FOLDER exists or create it if not
+rm -rf "$FAVICON_FOLDER"
+mkdir -p "$FAVICON_FOLDER"
+
 if [[ $1 =~ \.(jpg|jpeg|png)$ ]]; then
-  SOURCE_IMAGE="$FAVICON_FOLDER/$1"
+  SOURCE_IMAGE="$1"
 else
   echo "Error: Only JPEG and PNG files are supported."
   exit 1
 fi
 
-SOURCE_IMAGE="$FAVICON_FOLDER/web-app-manifest-512x512.png"
-rm "$FAVICON_FOLDER/browserconfig.xml" "$FAVICON_FOLDER/site.webmanifest"
+# Process the source image to generate various favicon sizes
 ffmpeg -i "$SOURCE_IMAGE" -vf "scale=16:16" "$FAVICON_FOLDER/favicon-16x16.png"
 ffmpeg -i "$SOURCE_IMAGE" -vf "scale=32:32" "$FAVICON_FOLDER/favicon-32x32.png"
 ffmpeg -i "$SOURCE_IMAGE" -vf "scale=192x192" "$FAVICON_FOLDER/android-chrome-192x192.png"
-ffmpeg -i "$SOURCE_IMAGE" -vf "scale=512:512" "$FAVICON_FOLDER/android-chrome-512x512"
+ffmpeg -i "$SOURCE_IMAGE" -vf "scale=512:512" "$FAVICON_FOLDER/android-chrome-512x512.png"
 ffmpeg -i "$SOURCE_IMAGE" -vf "scale=150:150" "$FAVICON_FOLDER/mstile-150x150.png"
